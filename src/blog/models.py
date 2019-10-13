814092b17c357451e3c159fd1e4682a781b431ea
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-13 14:07:26 +0200

    create models

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,26 @@
+from datetime import date 
+
 from django.db import models
 
-# Create your models here.
+from organizer.models import Startup, Tag
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(max_length=63,
+        unique_for_month='pub_date',
+        help_text='A label for URL config')
+    text = models.TextField()
+    pub_date = models.DateField('date published',
+        default=date.today)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts')
+    startups = models.ManyToManyField(Startup, related_name='blog_posts')
+
+    class Meta:
+        get_latest_by = 'pub_date'
+        ordering = ['-pub_date', 'title']
+        verbose_name = 'blog post'
+
+    def __str__(self):
+        date_string = self.pub_date.strftime('%Y-%m-%d')
+        return f'{self.title} on {date_string}'
