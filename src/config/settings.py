a71bc8c37b3edf880972f076511e55af1767901a
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-13 13:15:05 +0200

    setup stuff

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local 
+    'blog.apps.BlogConfig',
+    'organizer.apps.OrganizerConfig',
 ]
 
 MIDDLEWARE = [
