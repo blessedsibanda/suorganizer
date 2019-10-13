b5bd5ac69c279989a014a2a4d918984339f2519c
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-13 19:22:18 +0200

    simple view

---
+++ src/organizer/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import HelloWorld
+
+
+urlpatterns = [
+    path("", HelloWorld.as_view(), name="hello")
+]
--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from django.views import View
 
-# Create your views here.
+
+class HelloWorld(View):
+    def get(self, request):
+        return HttpResponse("Hello blessed world!")    
+
