582cd087dc128a8e08cbd1757403974348d43ef7
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-13 19:52:40 +0200

    tag list and detail views

--- src/organizer/urls.py
+++ src/organizer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HelloWorld
+from .views import TagApiList, TagApiDetail
 
 
 urlpatterns = [
-    path("", HelloWorld.as_view(), name="hello")
+    path("", TagApiList.as_view(), name="api-tag-list"),
+    path("<int:pk>/", TagApiDetail.as_view(), name="api-tag-detail"),
 ]
