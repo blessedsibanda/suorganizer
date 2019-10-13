93ceaa7b82ad2afcce34bb032018effc282b82a7
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-13 16:11:15 +0200

    register models in the admin

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,4 +1,4 @@
-from datetime import date 
+from datetime import date
 
 from django.db import models
 
@@ -7,20 +7,19 @@ from organizer.models import Startup, Tag
 
 class Post(models.Model):
     title = models.CharField(max_length=63)
-    slug = models.SlugField(max_length=63,
-        unique_for_month='pub_date',
-        help_text='A label for URL config')
+    slug = models.SlugField(
+        max_length=63, unique_for_month="pub_date", help_text="A label for URL config"
+    )
     text = models.TextField()
-    pub_date = models.DateField('date published',
-        default=date.today)
-    tags = models.ManyToManyField(Tag, related_name='blog_posts')
-    startups = models.ManyToManyField(Startup, related_name='blog_posts')
+    pub_date = models.DateField("date published", default=date.today)
+    tags = models.ManyToManyField(Tag, related_name="blog_posts")
+    startups = models.ManyToManyField(Startup, related_name="blog_posts")
 
     class Meta:
-        get_latest_by = 'pub_date'
-        ordering = ['-pub_date', 'title']
-        verbose_name = 'blog post'
+        get_latest_by = "pub_date"
+        ordering = ["-pub_date", "title"]
+        verbose_name = "blog post"
 
     def __str__(self):
-        date_string = self.pub_date.strftime('%Y-%m-%d')
-        return f'{self.title} on {date_string}'
+        date_string = self.pub_date.strftime("%Y-%m-%d")
+        return f"{self.title} on {date_string}"
--- src/organizer/models.py
+++ src/organizer/models.py
@@ -1,53 +1,53 @@
 from django.db import models
-from django_extensions.db.fields import AutoSlugField   
+from django_extensions.db.fields import AutoSlugField
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=31, unique=True)
-    
+
     slug = AutoSlugField(
-        help_text= 'A label for URL config',   
-        max_length=31,
-        populate_from=['name'],
+        help_text="A label for URL config", max_length=31, populate_from=["name"]
     )
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
 
     def __str__(self):
-        return self.name 
+        return self.name
 
 
 class Startup(models.Model):
     name = models.CharField(max_length=31, db_index=True)
-    slug = models.SlugField(max_length=31, unique=True,
-        help_text='A label for URL config')
+    slug = models.SlugField(
+        max_length=31, unique=True, help_text="A label for URL config"
+    )
     description = models.TextField()
-    founded_date = models.DateField('date founded')
+    founded_date = models.DateField("date founded")
     contact = models.EmailField()
     website = models.URLField(max_length=255)
     tags = models.ManyToManyField(Tag)
 
     class Meta:
-        get_latest_by = 'founded_date'
-        ordering = ['name']
+        get_latest_by = "founded_date"
+        ordering = ["name"]
 
     def __str__(self):
-        return self.name 
+        return self.name
 
 
 class NewsLink(models.Model):
     title = models.CharField(max_length=31)
     slug = models.SlugField(max_length=31)
-    pub_date = models.DateField('date published')
+    pub_date = models.DateField("date published")
     link = models.URLField(max_length=255)
     startup = models.ForeignKey(Startup, on_delete=models.CASCADE)
 
     class Meta:
-        get_latest_by = 'pub_date'
-        ordering = ['-pub_date']
-        unique_together = ('slug', 'startup')
-        verbose_name = 'news article'
+        get_latest_by = "pub_date"
+        ordering = ["-pub_date"]
+        unique_together = ("slug", "startup")
+        verbose_name = "news article"
 
     def __str__(self):
-        return f'{self.startup}: {self.title}'
+        return f"{self.startup}: {self.title}"
+
