f12fb43c2ec6b20b4022b60a5ceeeeb6a6d2bfcb
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-14 09:54:11 +0200

    use generics

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,50 +1,30 @@
 import json
 
 from django.views.decorators.http import require_http_methods
-from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework.response import Response 
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Tag, Startup
 from .serializers import TagSerializer, StartupSerializer
 
 
-class TagApiDetail(APIView):
-    def get(self, request, slug):
-        tag = get_object_or_404(Tag, slug=slug)
-        s_tag = TagSerializer(
-            tag,
-            context={'request': request}
-        )
-        return Response(s_tag.data)    
+class TagApiDetail(RetrieveAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    lookup_field = 'slug'
 
 
-class TagApiList(APIView):
-    def get(self, request):
-        tag_list = get_list_or_404(Tag)
-        s_tags = TagSerializer(
-            tag_list, many=True,
-            context={'request': request}
-        )
-        return Response(s_tags.data)
+class TagApiList(ListAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
 
 
-class StartupAPIDetail(APIView):
-    def get(self, request, slug):
-        startup = get_object_or_404(Startup, slug=slug)
-        s_startup = StartupSerializer(
-            startup,
-            context={ 'request': request }
-        )
-        return Response(s_startup.data)
+class StartupAPIDetail(RetrieveAPIView):
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
+    lookup_field = 'slug'
 
 
-class StartupAPIList(APIView):
-    def get(self, request):
-        startup_list = get_list_or_404(Startup)
-        s_startup = StartupSerializer(
-            startup_list,
-            many=True,
-            context = {'request': request }
-        )
-        return Response(s_startup.data)
+class StartupAPIList(ListAPIView):
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
