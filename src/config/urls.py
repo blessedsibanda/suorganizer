99615639c9675581b3252486a806d66ed1042c5e
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-14 10:41:46 +0200

    version api urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+api_urls = [
+    path("", include("organizer.routers")),
+    path("", include("blog.routers")),
+]
+
 urlpatterns = [
     path("admin/", admin.site.urls), 
-    path("", include("organizer.urls")),
-    path("", include("blog.urls")),
+    path('api/v1/', include(api_urls)),
 ]
