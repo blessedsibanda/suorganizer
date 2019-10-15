10b2f2c84c3d3e5086ca86a153a7e77d727bfdb9
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-15 12:42:48 +0200

    tag views

--- src/organizer/urls.py
+++ src/organizer/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import TagList, TagDetail, StartupList, StartupDetail
+from .views import (TagList, TagDetail, StartupList, 
+    StartupDetail, TagDelete, TagCreate, TagUpdate)
 
 urlpatterns = [
     path("tag/", TagList.as_view(), name="tag_list"),
+    path("tag/create/", TagCreate.as_view(), name="tag_create"),
     path("tag/<str:slug>/", TagDetail.as_view(), name="tag_detail"),
+    path("tag/<str:slug>/update/", TagUpdate.as_view(), name="tag_update"),
+    path("tag/<str:slug>/delete/", TagDelete.as_view(), name="tag_delete"),
     path("startup/", StartupList.as_view(), name="startup_list"),
     path("startup/<str:slug>/", StartupDetail.as_view(),
         name="startup_detail"),
